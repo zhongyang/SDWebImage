5cb35645febf1ace0fcc0047f2da6448d5aac39e
zhongyang <yangzhong@cienet.com.cn>
2015-01-02 14:36:05 +0800

    Fixed image decode bug in SDWebImage
    If picture's dimension over 1000px,  SDWebImageDecoder will cost large vm to decode image, and this will cause crash in low memory device,such as iphone4, 4s

--- SDWebImage/SDWebImageDecoder.m
+++ SDWebImage/SDWebImageDecoder.m
@@ -9,10 +9,19 @@
  */
 
 #import "SDWebImageDecoder.h"
+#define MAX_IMAGE_SIZE 1000
 
 @implementation UIImage (ForceDecode)
++ (CGSize) originalSize:(UIImage*) image
+{
+    return  CGSizeMake(CGImageGetWidth(image.CGImage), CGImageGetHeight(image.CGImage));
+}
 
 + (UIImage *)decodedImageWithImage:(UIImage *)image {
+    return [self decodedImageWithImage:image maxSize:MAX_IMAGE_SIZE];
+}
+
++ (UIImage *)decodedImageWithImage:(UIImage *)image maxSize:(NSInteger) maxSize{
     if (image.images) {
         // Do not decode animated images
         return image;
@@ -20,6 +29,7 @@
 
     CGImageRef imageRef = image.CGImage;
     CGSize imageSize = CGSizeMake(CGImageGetWidth(imageRef), CGImageGetHeight(imageRef));
+    imageSize = [self checkMaxImageSize:imageSize maxSize:maxSize];
     CGRect imageRect = (CGRect){.origin = CGPointZero, .size = imageSize};
 
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
@@ -67,6 +77,21 @@
     UIImage *decompressedImage = [UIImage imageWithCGImage:decompressedImageRef scale:image.scale orientation:image.imageOrientation];
     CGImageRelease(decompressedImageRef);
     return decompressedImage;
+}
+
++(CGSize) checkMaxImageSize:(CGSize) size maxSize:(NSInteger) max
+{
+    CGFloat whRatio = size.width / size.height;
+    if (size.height > max || size.width > max) {
+        if (size.height > size.width) {
+            size.width = max * whRatio;
+            size.height = max;
+        }else{
+            size.width = max ;
+            size.height = max / whRatio;
+        }
+    }
+    return size;
 }
 
 @end
